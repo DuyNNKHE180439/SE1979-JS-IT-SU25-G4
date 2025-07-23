83e3bc0db74145a5d0666a661aec0d79dd738352
DuyNNKHE180439 <duynnkhe180439@fpt.edu.vn>
2025-07-23 23:35:35 +0700

    done dashboard

--- src/java/controller/AdminDashboard.java
+++ src/java/controller/AdminDashboard.java
@@ -2,76 +2,53 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller;
 
+import dao.ReportDAO;
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+import model.Room;
 
 /**
  *
  * @author ADMIN
  */
-@WebServlet(name="AdminDashboard", urlPatterns={"/admin-dashboard"})
+@WebServlet(name = "AdminDashboard", urlPatterns = {"/admin-dashboard"})
 public class AdminDashboard extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AdminDashboard</title>");  
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AdminDashboard at " + request.getContextPath () + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    } 
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
-     * Handles the HTTP <code>GET</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        request.getRequestDispatcher("admin-dashboard.jsp").forward(request, response);    } 
+            throws ServletException, IOException {
+        ReportDAO dao = new ReportDAO();
+        List<Room> roomList = dao.getAllRooms();
+        request.setAttribute("roomList", roomList);
+        int totalRegistrations = dao.getTotalRegistrations();
+        request.setAttribute("totalRegistrations", totalRegistrations);
+        int totalRooms = dao.getTotalRooms();
+        request.setAttribute("totalRooms", totalRooms);
+        BigDecimal totalRevenue = dao.getTotalRevenue();
+        request.setAttribute("totalRevenue", totalRevenue);
+        int totalOccupants = dao.getTotalOccupants();
+        request.setAttribute("totalOccupants", totalOccupants);
 
-    /** 
-     * Handles the HTTP <code>POST</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
+        Map<String, Integer> roleCountMap = dao.getUserCountByRole();
+
+        request.setAttribute("roleCountMap", roleCountMap);
+        Map<String, Integer> statusCounts = dao.getUserCountByStatus();
+        request.setAttribute("statusCounts", statusCounts);
+        request.getRequestDispatcher("admin-dashboard.jsp").forward(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
